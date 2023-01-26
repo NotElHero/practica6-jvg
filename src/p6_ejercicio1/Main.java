0b80d5737ec64912ab8d7ea6d4443a4da5d84fb7
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2023-01-26 12:39:45 +0100

    Initial commit

---
+++ src/p6_ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package p6_ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Suma suma = new Suma();
+        suma.sumarNumeros();
+    }
+}
---
+++ src/p6_ejercicio1/Suma.java
@@ -0,0 +1,19 @@
+package p6_ejercicio1;
+
+import java.util.Scanner;
+
+public class Suma {
+    Scanner sc = new Scanner(System.in);
+
+    public int sumarNumeros(){
+        int n1, n2;
+        int suma = 0;
+
+        System.out.println("Introduce el primer numero: ");
+        n1 = sc.nextInt();
+
+        suma = n1 /*+ n28*/;
+
+        return suma;
+    }
+}
