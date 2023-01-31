c98921667df5b118ec6d9effe4c8d6f22bf58fbe
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2023-01-31 12:37:33 +0100

    v1.4

--- src/p6_ejercicio1/Suma.java
+++ src/p6_ejercicio1/Suma.java
@@ -5,18 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Suma {
-    public static void main(String[] args) throws IOException {
-        int n1, n2;
+    public static void main(String[] args) {
+        String cadenan1 = null, cadenan2 = null;
+        int n1 = 0, n2 = 0;
 //        Boolean n1isNumeric = true;
 //        Boolean n2isNumeric = true;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
-            System.out.println("Primer numero");
-            n1 = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("Segundo numero");
-            n2 = Integer.parseInt(bufferedReader.readLine());
+            while (cadenan1.equals(null) && cadenan1 == String.valueOf(cadenan1)) {
+                System.out.println("Primer numero");
+                cadenan1 = bufferedReader.readLine();
 
+                for (int i = 0; i < cadenan1.length(); i++){
+                    char c = cadenan1.charAt(i);
+                    if (c < '0' || c > '9') {
+
+                    }
+                }
+            }
+
+            if (!(Integer.valueOf(n2) < 0) && !(Integer.valueOf(n2) > 9)) {
+                System.out.println("Segundo numero");
+                n2 = Integer.parseInt(bufferedReader.readLine());
+            }
             System.out.println(n1 + n2);
         } catch (IOException ioe){
             ioe.printStackTrace();
